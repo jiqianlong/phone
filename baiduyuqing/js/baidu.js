27bff0a63d3d0a2aee705ab97bc1c83194dddc0f
jiqianlong <1003035341@qq.com>
2016-12-01 00:42:43 +0800

    sd

--- baiduyuqing/js/baidu.js
+++ baiduyuqing/js/baidu.js
@@ -56,7 +56,95 @@ $(function () {
     //检测动画是否完成
     $(".box")[0].addEventListener("webkitTransitionEnd",function () {
         falg=true;
+       $(".dong").each(function (index,obj) {
+           if (num==0){
+               // $(".left").eq(index).css({
+               //     transform:"translate(0,0)",
+               //     opacity:0
+               // })
+               // $(".right").eq(index).css({
+               //     transform:"translate(0,0)",
+               //     opacity:0
+               // })
+               return ;
+           }
+           if(num!=0){
+               if (index==num){
+                   $(".left").eq(index).css({
+                       transform:"translate(0,0)",
+                       opacity:0
+                   })
+                   $(".right").eq(index).css({
+                       transform:"translate(0,0)",
+                       opacity:0
+                   })
+               }else{
+                   $(".left").eq(index).css({
+                       transform:"translate(50px,0)",
+                       opacity:1
+                   })
+                   $(".right").eq(index).css({
+                       transform:"translate(-50px,0)",
+                       opacity:1
+                   })
+               }
+           }
+       })
     })
+    //菜单控制
+    var falg1=true;
+    $(".menu").click(function () {
+        if(falg1){
+            $(this).find(".menu-tline").css({
+                transform:"translate(0,4px) rotate(45deg)"
+            })
+            $(this).find(".menu-bline").css({
+                transform:"translate(0,-4px) rotate(-45deg)"
+            })
+            $(".dh-min a").each(function(index,obj){
+                $(obj).css({
+                    opacity:0,animation:"menu 1s ease forwards "+index*0.2+"s"
+                })
+            })
+            $(".dh-min").css({display:"block"})
+            falg1=false;
+        }else{
+            $(this).find(".menu-tline").css({
+                transform:"translate(0,0) rotate(0deg)"
+            })
+            $(this).find(".menu-bline").css({
+                transform:"translate(0,0) rotate(0deg)"
+            })
+            $(".dh-min a").each(function(index,obj){
+                $(obj).css({
+                    opacity:1,animation:"menu1 1s ease forwards "+(1.2-index*0.2)+"s",
+                })
+            })
+            falg1=true;
+            setTimeout(function () {
+                $(".dh-min").css({display:"none"})
+            },1700)
+        }
+    })
+    $(window).resize(function () {
+        var w=$(window).width();
+        h=$(window).height();
+        $(".box").css({
+            marginTop:-num*h,
+            transition:"margin-top 1s ease"
+        })
+        if(w>1000){
+            $(".dh-min a").css({
+                animation:"none",
+                opacity:0
+            })
+            $(".menu-tline,.menu-bline").css({
+                transform:"translate(0,0) rotate(0deg)"
+            })
+            falg1=true;
+        }
+    })
+
 
 
 
